c3e90e6f7d493e96a28ab7332d742b92a8c30cf6
SmartHomeBastler <83531755+MaxxKra@users.noreply.github.com>
2025-04-11 20:25:30 +0200

    Update __init__.py

--- custom_components/puppenhaus_weather/__init__.py
+++ custom_components/puppenhaus_weather/__init__.py
@@ -1,1 +1,11 @@
-# __init__.py
+"""Init for Puppenhaus Wetter platform."""
+
+DOMAIN = "puppenhaus_weather"
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Old-style platform setup (unused here)."""
+    pass
+
+def setup(hass, config):
+    """Set up is called when Home Assistant is loading config."""
+    return True
